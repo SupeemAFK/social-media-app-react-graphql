98e42ef351708af5595abac08022ca6301c8484d
Peemmaphat Sripongsai <83326313+SupeemAFK@users.noreply.github.com>
2023-04-29 03:12:18 +0700

    Update useUploadImg.js

--- client/hooks/useUploadImg.js
+++ client/hooks/useUploadImg.js
@@ -6,10 +6,10 @@ export default function useUploadImg() {
     async function uploadImg(file) {
         const formData = new FormData();
         formData.append('file', file);
-        formData.append('upload_preset', 'kurkzobi')
+        formData.append('upload_preset', 'PRESER_NAME')
         try {
             setIsUploading(true);
-            const res = await fetch('https://api.cloudinary.com/v1_1/dkmwuwsvw/image/upload', { method: 'POST', body: formData })
+            const res = await fetch('CLOUNDINARY_ENDPOINT', { method: 'POST', body: formData })
             const data = await res.json();
             return data.url;
             
